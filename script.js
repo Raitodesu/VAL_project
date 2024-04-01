85bd93b3076658e9e207e709837730ad054f3f9e
Raitodesu <116088407+Raitodesu@users.noreply.github.com>
2024-04-01 21:26:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     const menu_btn = document.querySelector('.hamburger-menu');
-    const nav_menu = document.querySelector('.nav');
+    const nav_menu = document.querySelector('.navcontainer');
     menu_btn.addEventListener('click', function(){
         menu_btn.classList.toggle('is-active');
         nav_menu.classList.toggle('is-active');
