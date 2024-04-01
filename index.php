06b2e15ce45869633b71ad923db86a79ae8b3fa3
Raitodesu <116088407+Raitodesu@users.noreply.github.com>
2024-04-01 13:53:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Caraga State University Queue System</title>
+</head>
+<body>
+    <header>
+        <div class="navdiv">
+            <div class="logo"><a href="#"><img src="picture/logo.png" alt="Logo" height="50px" width="200px"></a></div>
+            <div class="hamburger-menu">
+                <div class="bar"></div>
+            </div>
+        </div>
+
+        <div class="nav">
+            <ul>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Log in</a></li>
+                <li><a href="#">List Of Queue</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul> 
+        </div>
+    </header>
+
+    <div class="content">
+        
+    </div>
+    <div class="foota">
+        <footer>
+            <p class="p1">
+            &copy; <span style="color:#56AF2E">CARAGA STATE UNIVERSITY</span> - CABADBARAN CAMPUS 2024
+            </p>
+
+            <p class="p2">
+            Created by VAL Developers
+            </p>
+        </footer>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    const menu_btn = document.querySelector('.hamburger-menu');
+    const nav_menu = document.querySelector('.nav');
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        nav_menu.classList.toggle('is-active');
+    });
+}
