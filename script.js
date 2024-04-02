54ddb64b8ba8cb59e0d898ea7e86742211a0799c
Raitodesu <116088407+Raitodesu@users.noreply.github.com>
2024-04-02 16:01:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 window.onload = function(){
     const menu_btn = document.querySelector('.hamburger-menu');
     const nav_menu = document.querySelector('.navcontainer');
-    menu_btn.addEventListener('click', function(){
+    const blur_bg = document.querySelector('.blur-background')
+    
+    function toggleActive(){
         menu_btn.classList.toggle('is-active');
         nav_menu.classList.toggle('is-active');
+        blur_bg.classList.toggle('is-active');
+    }
+    
+    function toggleDeactivate(){
+        menu_btn.classList.remove('is-active')
+        blur_bg.classList.remove('is-active')
+        nav_menu.classList.remove('is-active')
+    }
+
+    menu_btn.addEventListener('click', function(){
+        toggleActive();
     });
+
+    blur_bg.addEventListener('click', function(){
+        toggleDeactivate();
+    })
 }
